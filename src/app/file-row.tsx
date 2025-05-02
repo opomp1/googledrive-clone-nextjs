d7b1797eadbbf69c97d75106b7e7aefe17da96bb
Tirawat Pompakunsawat <bamiowen@gmail.com>
2025-05-02 16:12:11 +1000

    Drive content being rendered

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,8 +1,9 @@
 import type { File, Folder } from "~/lib/mock-data";
 
 import { Folder as FolderIcon, FileIcon } from "lucide-react";
+import type { files, folders } from "~/server/db/schema";
 
-export function FileRow(props: { file: File }) {
+export function FileRow(props: { file: typeof files.$inferSelect }) {
   const { file } = props;
   return (
     <li
@@ -28,7 +29,7 @@ export function FileRow(props: { file: File }) {
 }
 
 export function FolderRow(props: {
-  folder: Folder;
+  folder: typeof folders.$inferSelect;
   handleFolderClick: () => void;
 }) {
   const { folder, handleFolderClick } = props;
